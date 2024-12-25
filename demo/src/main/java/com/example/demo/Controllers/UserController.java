a3ac7d87f7b32af7b08ac659c257281bf3946ef2
KavinduNuwandika <kavindunuwandika@gmail.com>
2024-12-25 19:45:35 +0530

    Enable User Controllers to Handle Mysql Data Base Saves
    Enable User Controllers to Handle Mysql Data Base Saves

--- demo/src/main/java/com/example/demo/Controllers/UserController.java
+++ demo/src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,13 +1,34 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String homePage() {
         return "HomePage";
+    }
+
+    @GetMapping("/SignUp")
+    public String dataEntryPage(Model model) {
+        model.addAttribute("user", new User());
+        return "SignUp";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/";
     }
 }
---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,85 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private LocalDate dob;
+    private String phone;
+    private String email;
+    private String password;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
