e948c4e2f3b862f865d0906c11366b5531afa1ee
KavinduNuwandika <kavindunuwandika@gmail.com>
2024-12-31 22:26:20 +0530

    Update Docker

--- STUDENT_MANAGMENT/src/test/java/com/GSCOMP230/Student_Managment/DemoApplicationTests.java
+++ STUDENT_MANAGMENT/src/test/java/com/GSCOMP230/Student_Managment/DemoApplicationTests.java
@@ -3,11 +3,10 @@ package com.GSCOMP230.Student_Managment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)  // Explicitly specifying the main application class
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
